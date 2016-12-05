97c695887748efbbd35c7bfb9d305113f33b9f71
Joe Ponzio <joe@newnine.com>
2016-12-05 15:18:44 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# wordpress-installer
-Use NPM to install the latest copy of WordPress
+# WordPress Installer
+The WordPress Installer lets you quickly install the latest (or any past) version WordPress. **NOTE:** This does not include WordPress MU versions prior to WordPress 3.0 when MU was merged into WordPress.
+
+## How To Use
+
+* `git clone git@github.com:joeponzio/wordpress-installer.git`
+* `npm run installer`
+* When prompted, enter the version of WordPress to install (or leave blank for the latest stable version)
+
+## What To Expect
+
+1. The desired version of **WordPress** will be downloaded and installed in the current directory (this may take a minute so be patient)
+1. The installer will remove its own files as they're not needed anymore
+1. You have a clean install of WordPress. Enjoy!
+
+(You still need to create your `wp-config.php` to connect to the database.)
+
+## Got Ideas? Go Fork Yourself
+I didn't want this installer to be too opinionated; so, it is as Plain Vanilla as they come. We use modified versions of it to:
+
+* Auto-generate `wp-config.php` files with database credentials filled in during the install
+* Delete *hello.php* and *akismet* upon install
+* Clone our framework and base child theme repos (and delete the `git` references so they're fresh)
+* Create a new `git` repo to the project (and add everything, commit with "initial commit" and push to Github :)
+* Run `npm install` and `bower install` after the installation so we're quickly up and running with SCSS and livereload
+
+The point is: you can use this to do whatever you want. Use it as a basis for making your own, custom WordPress installer. Simply fork this repo and customize to your heart's content.
+
+## Issues or Problems
+If you run into any problems or issues with this installer, you should [post them in the issues section of this repo](https://github.com/joeponzio/wordpress-installer/issues).
---
+++ index.js
@@ -0,0 +1,92 @@
+#!/usr/bin/env node
+var AdmZip = require( 'adm-zip' );
+var fs     = require( 'fs' );
+var https  = require( 'https' );
+var ncp    = require( 'ncp' ).ncp;
+var prompt = require( 'prompt' );
+var wpv;
+
+ncp.limit = 16;
+
+var deleteFolderRecursive = function( path ){
+	if( fs.existsSync( path ) ){
+		fs.readdirSync( path ).forEach( function( file, index ){
+			var curPath = path + '/' + file;
+			if( fs.lstatSync( curPath ).isDirectory() ){
+				deleteFolderRecursive( curPath );
+			} else {
+				fs.unlinkSync( curPath );
+			}
+		});
+		fs.rmdirSync( path );
+	}
+};
+
+var downloadWordPress = function(){
+	var wpurl;
+	if( !wpv.length ){
+		wpurl = 'https://wordpress.org/latest.zip';
+	} else {
+		wpurl = 'https://wordpress.org/wordpress-' + wpv + '.zip';
+	}
+	https.get( wpurl, function( response ){
+		console.log( 'Downloading WordPress. Please be patient as this may take a minute.' );
+		response.on( 'data', function( data ){
+			fs.appendFileSync( 'wordpress.zip', data );
+		});
+		response.on( 'end', function(){
+			console.log( 'Extracting WordPress' );
+			fs.unlinkSync( 'LICENSE' );
+			fs.unlinkSync( 'README.md' );
+			var zip = new AdmZip( 'wordpress.zip' );
+			zip.extractAllTo( './' );
+			fs.unlink( 'wordpress.zip', moveWordPress );
+		})
+	});
+};
+
+var moveWordPress = function(){
+	ncp( 'wordpress', '', function( err ){
+		deleteFolderRecursive( 'wordpress' );
+		wipeInitializer();
+	});
+};
+
+var wipeInitializer = function(){
+	console.log( 'Cleaning up the installer.' );
+	deleteFolderRecursive( 'node_modules' );
+	fs.unlinkSync( '.gitignore' );
+	fs.unlinkSync( 'index.js' );
+	fs.unlinkSync( 'package.json' );
+	console.log( 'All set. Have fun!' );
+	return 1;
+};
+
+fs.access( './index.php', fs.F_OK, function( err ){
+	if( !err ){
+		console.log( 'WordPress (or something else) is already installed in this directory. You must run the installer in its own directory.' );
+		return 1;
+    }
+});
+
+deleteFolderRecursive( '.git' );
+
+prompt.start();
+var promptSchema = {
+	properties: {
+		wpv: {
+			description: 'Version Number (leave blank for the latest version)',
+			pattern: /^[0-9a-zA-Z\-\.]+$/,
+			message: 'Version number must be only letters, numbers, dashes, and/or hyphens',
+			required: false
+		}
+    }
+};
+
+prompt.get( promptSchema, function( err, result ){
+	if( err ){
+		console.log( err );
+	}
+	wpv = result.wpv;
+	downloadWordPress();
+} );
